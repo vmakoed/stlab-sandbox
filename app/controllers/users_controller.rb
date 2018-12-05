f881e62a940e99c8369eef0fad8a002bdd1ec127
Victor Makoed <vmakoed@gmail.com>
2018-12-05 22:44:51 +0300

    Initial commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,38 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+
+    if @user.persisted?
+      redirect_to action: :index
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(user_params)
+    redirect_to action: :index
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name,
+                                  :last_name, :email)
+  end
 end
